0fb1db9726eeb7b4468e4c22b9776ed088e190ca
Pedro <pedrolosasp@gmail.com>
2022-12-31 09:31:19 +0100

    POC: Minor fixing.

--- src/config/mongo.db.js
+++ src/config/mongo.db.js
@@ -4,7 +4,10 @@ const logger = require('npmlog');
 module.exports.connectMongoDb = async (uri) => {
   try {
     mongoose.set('strictQuery', true);
-    const db = await mongoose.connect(uri, {});
+    const db = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     logger.info('Database mongo:', db.connections[0].name);
     return db;
   } catch (error) {
